660005942521493e1b5c0c36a39b027074e9e4b6
AakritiDasgupta <dasgupta4897@mydusd.org>
2019-11-19 16:28:08 +0000

    My First Commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Insert Name & Period Here]</p>'; ?> 
+    
+  <ul class="nav nav-pills nav-fill">
+    <li class="nav-item">
+      <a class="nav-link active" href="index.html">Index.html</a>
+    </li>
+    <li class="nav-item">
+     <a class="nav-link" href="Dasgupta_AboutMe.html">AboutMe.html
+     </a>
+    </li>
+    <li> 
+      <a class="nav-link" href="Dasgupta_Projects.html">Projects.html
+      </a>
+    </li>
+    <li >
+       <a class="nav-link" href="Dasgupta_Resume.html">Resume.html
+       </a>
+    </li>
+    <li>
+        <a class="nav-link" href= "Dasgupta_ContactMe.html" tabindex="-1" aria-disabled="true">ContactMe.html
+       </a>
+    </li>
+  </ul >
+  </body>
+</html>
